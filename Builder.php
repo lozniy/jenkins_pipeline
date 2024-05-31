23b922d328aada9f60112b1a5c2b4ea832e1bca0
lozniy <116027252+lozniy@users.noreply.github.com>
2024-05-31 10:32:23 +0300

    Create Builder.php

---
+++ Builder.php
@@ -0,0 +1,40 @@
+<?php
+
+interface builder
+{
+    public function build();
+}
+
+class SbbolBuilder implements builder
+{
+    public function build()
+    {
+        echo 'Sbbol builder build a request';
+    }
+}
+
+class SberIdBuilder implements builder
+{
+    public function build()
+    {
+        echo 'SberId builder build a request';
+    }
+}
+
+class Manager
+{
+    public function __construct(
+        private builder $builder
+    )
+    {}
+
+    public function getRequest()
+    {
+        $this->builder->build();
+    }
+}
+
+//$builder = new SbbolBuilder();
+$builder = new SberIdBuilder;
+$manager = new Manager($builder);
+$manager->getRequest();
